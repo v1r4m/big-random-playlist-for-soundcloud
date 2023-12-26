13371725a0a598936e23c6fbb0ff7860810a20b8
v1r4m <choyoung7789@gmail.com>
2023-12-26 17:43:02 +0900

    nextjs 공식문서를 읽자 ^^ 빵긋

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # big-random-playlist-for-soundcloud
+* make them use their own soundcloud cookie! 
+* 아니 파일가져다 쓰지않고 BE-FE 스트리밍하겠다는게 그렇게 ! 큰! 욕심인가 진짜 너무힘들다
--- big-random-playlist-for-soundcloud/src/app/viram/page.tsx
+++ big-random-playlist-for-soundcloud/src/app/viram/page.tsx
@@ -1,23 +1,68 @@
 'use client';
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useRef } from 'react';
 
 const PlayApp = () => {
-  const [playlist, setPlaylist] = useState(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    const fetchPlaylist = async () => {
-        const response = await axios.get('/api/soundcloud');
-        console.log(response.data);
-        setPlaylist(response.data);
-    };
+    if (window.MediaSource) {
+      const mediaSource = new MediaSource();
+      let sourceBuffer: SourceBuffer | null = null;
 
-    fetchPlaylist();
+      mediaSource.addEventListener('sourceopen', async () => {
+        sourceBuffer = mediaSource.addSourceBuffer('audio/mpeg');
+        await fetchPlaylist();
+      });
+
+      const fetchPlaylist = async () => {
+        try {
+          const response = await fetch('/api/soundcloud');
+          console.log(response);
+          await processChunkedResponse(response);
+          onChunkedResponseComplete();
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      if (audioRef.current) {
+        audioRef.current.src = URL.createObjectURL(mediaSource);
+      }
+
+      function onChunkedResponseComplete() {
+        console.log('all done!');
+      }
+
+      async function processChunkedResponse(response: Response) {
+        const reader = response.body!.getReader();
+
+        while (true) {
+          const result = await reader.read();
+          if (result.done) {
+            if (sourceBuffer && !sourceBuffer.updating) {
+              sourceBuffer.addEventListener('updateend', () => {
+                if (!sourceBuffer!.updating) {
+                  mediaSource.endOfStream();
+                }
+              });
+            }
+            console.log('returning');
+            break;
+          } else {
+            if (sourceBuffer && !sourceBuffer.updating) {
+              sourceBuffer.appendBuffer(result.value);
+            }
+            console.log('recursing');
+          }
+        }
+      }
+    }
   }, []);
 
   return (
-    <div> Hello world~! </div>
+    <div>
+      <audio controls ref={audioRef} />
+    </div>
   );
 };
 
